78fb6128498f245d7623406a0785ff6bbb6026e3
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-03-16 20:11:29 +0800

    routing metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,15 +1,39 @@
 import { notFound } from "next/navigation";
-export default function ProductList({
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+// export const generateMetadata = ({ params }: Props): Metadata => {
+//   return {
+//     title: `Product ${params.productId}`,
+//   };
+// };
+
+//async function
+export const generateMetadata = async ({
   params,
-}: {
-  params: { productId: string };
-}) {
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Iphone ${params.productId}`);
+    });
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductList({ params }: Props) {
   if (parseInt(params.productId) > 1000) {
     return notFound();
   }
   return (
     <main>
-      <h1>Product {params.productId} Details</h1>
+      <h1>{params.productId} Details</h1>
     </main>
   );
 }
