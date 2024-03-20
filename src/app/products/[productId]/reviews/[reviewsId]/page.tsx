c56f09c40194c1945e825c2239b9eb8821ed3fc3
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-03-21 01:23:33 +0800

    Error Handling

--- src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,10 +1,20 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default function Reviews({
   params,
 }: {
   params: { productId: string; reviewsId: string };
 }) {
+  const random = getRandomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error Loading Reviews");
+  }
+
   if (parseInt(params.reviewsId) > 1000) {
     return notFound();
   }
