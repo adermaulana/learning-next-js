d1be65514ffe0ddf9799f62f19993ca1fad88c28
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-03-21 22:06:13 +0800

    Recovering from Errors

--- src/app/products/[productId]/reviews/[reviewsId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewsId]/error.tsx
@@ -1,5 +1,21 @@
 "use client";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <div>{error.message}</div>;
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <div>
+      {error.message}{" "}
+      <button
+        className="py-2 px-4 rounded text-slate-300 font-bold bg-red-700 hover:bg-red-300 duration-300 bo"
+        onClick={reset}
+      >
+        Try Again
+      </button>
+    </div>
+  );
 }
